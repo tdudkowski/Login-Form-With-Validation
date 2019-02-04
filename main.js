3e0833b19f33166263cc95ef3f8c0877cb832103
tdudkowski <tdudkowski@gmail.com>
2019-02-04 02:44:05 +0100

    added array of days and mail validation

--- main.js
+++ main.js
@@ -14,9 +14,31 @@ const btn3 = document.querySelector('.stage3 button');
 const stage4 = document.querySelector('.stage4');
 const btn4 = document.querySelector('.stage4 button');
 const colors = ['cce5ff', 'fff3cd', 'd4edda', 'f8d7da'];
-let i = 0;
+// let i = 0;
+let flag = false;
 let resultOfOperation;
-const jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+const jours = [{
+ jour: 'lundi',
+ nombre: 1
+}, {
+ jour: 'mardi',
+ nombre: 2
+}, {
+ jour: 'mercredi',
+ nombre: 3
+}, {
+ jour: 'jeudi',
+ nombre: 4
+}, {
+ jour: 'vendredi',
+ nombre: 5
+}, {
+ jour: 'samedi',
+ nombre: 6
+}, {
+ jour: 'dimanche',
+ nombre: 7
+}, ];
 
 // const changer = () => {
 //  if (i == colors.length) {
@@ -26,40 +48,45 @@ const jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dim
 //  stage1.style.backgroundColor = `#${hex}`;
 // }
 
-const final = () => {
+const final = () => { // to console.log
  console.log('finish')
 }
 
-const go4 = () => {
+const go4 = () => { // start lats stage
  stage4.style.visibility = 'visible';
- stage4.style.transform = 'translateY(18rem)';
+ stage4.style.transform = 'translateY(21rem)';
  stage4.style.transiton = 'all .5s';
  stage4.style.backgroundColor = '#cce5ff';
  stage3.style.backgroundColor = '#d4edda';
  btn4.addEventListener('click', final);
 }
 
-const clickDay = function () {
+const clickDay = function () { // get data from clicked jours and compares it with the numer of today
  const day = new Date();
- let numberOfToday = (day.getDay()) - 1;
- const numberChosen = jours.indexOf(this.textContent);
- if (numberOfToday < 0) {
-  numberOfToday = 6;
+ const numberOfToday = (day.getDay());
+ // const numberChosen = jours.indexOf(this.textContent); // whet it was just a string
+ let numberChosen = jours.find(nombre => nombre.jour === this.textContent);
+ // let numberChosen = jours.filter(nombre => { // first try, too complicated
+ //  return nombre.jour === this.textContent
+ // });
+ numberChosen = numberChosen.nombre;
+ if (numberOfToday == 0) {
+  numberOfToday = 7;
  }
  if (numberOfToday == numberChosen) {
-  go4();
+  go4(); // success!
  } else {
   console.log(this.textContent, numberChosen, numberOfToday);
  }
 }
 
-const joursDeLaDemaine = () => {
+const joursDeLaDemaine = () => { // creates clickable list, order should be randomized
  divJours.style.visibility = 'visible';
  for (let i = 0; i < jours.length; i++) {
-  const jour = document.createElement('div');
-  jour.textContent = `${jours[i]}`;
-  jour.addEventListener('click', clickDay)
-  divJours.appendChild(jour);
+  const jourDiv = document.createElement('div');
+  jourDiv.textContent = `${jours[i]['jour']}`;
+  jourDiv.addEventListener('click', clickDay)
+  divJours.appendChild(jourDiv);
  }
 }
 
@@ -67,7 +94,7 @@ const go3 = () => {
  console.log(resultOfOperation)
  if (result.value == resultOfOperation) {
   stage3.style.visibility = 'visible';
-  stage3.style.transform = 'translateY(12rem)';
+  stage3.style.transform = 'translateY(15rem)';
   stage3.style.transiton = 'all .5s';
   stage3.style.backgroundColor = '#cce5ff';
   stage2.style.backgroundColor = '#d4edda';
@@ -81,12 +108,27 @@ const go3 = () => {
  }
 }
 
+const mailValidation = () => {
+ if (input_email.value == "") return;
+ let mailString = input_email.value.split('@');
+ if (mailString[0].length == 0 || mailString[2]) return;
+ mailString = mailString[1].split('.');
+ mailString = mailString[mailString.length - 1].toLowerCase()
+ for (let i = 0; i < mailString.length; i++) {
+  if (!mailString[i].match(/[a-z]/i)) {
+   return;
+  }
+ }
+ flag = true;
+}
+
 const go2 = (e) => {
  e.preventDefault();
- if (checkbox.checked && input_email.value != 0) {
+ mailValidation();
+ if (checkbox.checked && flag) {
   console.log(checkbox.value);
   stage2.style.visibility = 'visible';
-  stage2.style.transform = 'translateY(6rem)';
+  stage2.style.transform = 'translateY(9rem)';
   stage2.style.transiton = 'all .5s';
   stage2.style.backgroundColor = '#cce5ff';
   stage1.style.backgroundColor = '#d4edda';
@@ -95,7 +137,7 @@ const go2 = (e) => {
   checkbox.disabled = true;
 
   const number1 = Math.floor(Math.random() * 10);
-  const operator = '+';
+  const operator = '+'; // should be randomized
   const number2 = Math.floor(Math.random() * 10);
   resultOfOperation = number1 + number2;
   operation.textContent = `${number1}${operator}${number2}`;
